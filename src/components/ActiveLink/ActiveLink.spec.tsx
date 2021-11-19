b2b9d9324c42568121742fc78572f9ef5df1442b
hitk1 <luispaulo.degini@gmail.com>
2021-11-18 22:29:46 -0300

    feat: primeiros testes

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,34 @@
+import { render } from '@testing-library/react'
+import { ActiveLink } from '.'
+
+jest.mock('next/router', () => {
+    return {
+        useRouter() {
+            return {
+                asPath: '/'
+            }
+        }
+    }
+})
+
+test('active link render correctly', () => {
+    const refDom = render(
+        <ActiveLink href='/' activeClassName="active">
+            <a>Home</a>
+        </ActiveLink>
+    )
+
+    //Test simples para verificar se o componente foi renderizado procurando por um elemento que contenha o text 'Home'
+    expect(refDom.getByText('Home')).toBeInTheDocument()
+})
+
+test('active link is receiving active class name', () => {
+    const refDom = render(
+        <ActiveLink href='/' activeClassName="active">
+            <a>Home</a>
+        </ActiveLink>
+    )
+
+    //Teste simples para verificar se o componente renderizado contem a classe 'active' com base nas regras do component
+    expect(refDom.getByText('Home')).toHaveClass('active')
+})
