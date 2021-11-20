4ed0089aa0a548175cf47958733ee15c8274e42b
hitk1 <luispaulo.degini@gmail.com>
2021-11-20 16:11:24 -0300

    refactor: refatoracoes diversas

---
+++ src/tests/pages/Home.spec.tsx
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react'
+
+//Os testes para as paginas em especifico do [Next]
+//tem de ser criados separados pra nao ter conflito com a api de roteamento do Next
+
+
